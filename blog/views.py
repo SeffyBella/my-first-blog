70519c54dcdcc138494baff0f5e9c40727a822a4
seffy <seffybella@gmail.com>
2017-11-10 21:16:50 +0000

    Added views tocreate/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -42,5 +42,5 @@ def	post_edit(request, pk):
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
-		form = PostForm(request.Post, instance=post)
+		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
